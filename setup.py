83ab263b4ea3ddfef26269c0ef392dd037d8df4d
Narang-Garima <garimanarang2407@gmail.com>
2025-05-21 01:03:46 -0400

    edit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="GARIMA NARANG",
+    author_email="garimanarang2407@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
